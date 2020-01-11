01f6ca617f859e4b4d69f865d2896f0f22b10449
BowlORamen33 <59752785+BowlORamen33@users.noreply.github.com>
2020-01-11 09:27:21 -0700

    Merge branch 'master' of https://github.com/bearboticsfrc/2020-sbeve

---
+++ src/main/include/RobotContainer.h
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/Command.h>
+
+#include "commands/ExampleCommand.h"
+#include "commands/DefaultDrive.h"
+#include "subsystems/ExampleSubsystem.h"
+#include "subsystems/Drivetrain.h"
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls).  Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
+ */
+class RobotContainer {
+ public:
+  RobotContainer();
+
+  frc2::Command* GetAutonomousCommand();
+  frc2::Command* GetTeleopCommand();
+
+ private:
+  // The robot's subsystems and commands are defined here...
+  ExampleSubsystem m_subsystem;
+  Drivetrain m_drivetrain;
+  ExampleCommand m_autonomousCommand;
+  DefaultDrive m_defaultDrive;
+
+  void ConfigureButtonBindings();
+};
